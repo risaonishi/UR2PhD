5737ce8e0883f24c2d8db99f280a8f1d7f33abf6
Risa Onishi <risaonishi24@gmail.com>
2024-10-13 22:46:24 -0700

    Add basis for prompting problem/not problem

--- problem_or_not/problem_or_not.py
+++ problem_or_not/problem_or_not.py
@@ -3,9 +3,23 @@ sys.path.append('../')
 
 import pandas as pd
 import GoogleGemini as gg
+gg.InitGoogleGemini()
 
 df = pd.read_csv('/Users/risaonishi/Downloads/CS/UR2PhD/csv/ucr_submissions.csv')
 df = df[['title','selftext']]
-print(df)
+df = df.sample(n=10, random_state=50)
+problems = []
 
-gg.AskGoogleGemini()
+for index, row in df.iterrows():
+    title = row['title']
+    body = row['selftext']
+    prompt = "Given this title:\n" + title + "\n And this body:\n" + str(body) + "\n Is this post a problem? Answer in only Yes/No"
+    print(prompt)
+    response = gg.AskGoogleGemini(prompt,force=True)
+    problems.append(response)
+    print('====='*20)
+
+
+df['Problem'] = problems
+df.to_csv('problem_submissions.csv', index=False)
+print(df)
